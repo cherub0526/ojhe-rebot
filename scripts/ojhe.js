32a3d329c2296452cb52c15adc33b0b63f83a6ec
Kun <cherub0526@gmail.com>
2016-07-10 22:56:17 +0800

    update

--- scripts/ojhe.js
+++ scripts/ojhe.js
@@ -1,24 +1,6 @@
-// module.exports = function(robot){
-//     // robot.respond(/(.*)你(.*)/, function(res){
-//     //     res.send('關你屁事');
-//     // });
-
-//     robot.respond(/hello/, function(res){
-//         res.send('world');
-//     });
-// }
-
-module.exports = function(robot){
-    robot.respond(/check (.*)/, function(response){
-        var content = response.match[1];
-        if(content.indexOf('http')<0){
-            return response.send('不是一個正確的網址喔，你是不是忘記加 http 呢？');
-        }
-        robot.http(content).get()(function(err, res, body){
-            if(err){
-                return response.send(err);
-            }
-            response.send(res.statusCode+'');
-        })
-    });
+module.exports = function(robot) {
+  robot.hear(/(.*)/, function(response) {
+    re = ['0.0', '關你屁事'];
+    response.send(response.random(re));
+  });
 }
